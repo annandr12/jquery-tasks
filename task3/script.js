8ab667b7fa6647bc86f5aaf76ce15417ba596e75
Anna Andriushkina <annandr12@gmail.com>
2017-02-07 18:11:10 +0100

    beautify code

--- task3/script.js
+++ task3/script.js
@@ -1,21 +1,24 @@
+/*jshint esversion: 6 */
 $(document).ready(function() {
-  $.getJSON( "content.json", function( data ) {
-    var items = [];
-    var arr = [];
-    let div = "<div class='item col-xs-12 col-sm-6 col-md-6 col-lg-3'></div>"
-    console.log(data);
-    $.each( data, function( index, val ) {
-      let item  = $(div).html("<a href='" + val.url + "' title='" + val.description + "'><span class='image' style='background-image: url(" + val.url + ");'></span></a>");
-      console.log(val.description);
-      arr.push(item);
+    $.getJSON("content.json", function(data) {
+        let items = [];
+        let arr = [];
+        let div = "<div class='item col-xs-12 col-sm-6 col-md-6 col-lg-3'></div>";
+        console.log(data);
+        $.each(data, function(index, val) {
+            let item = $(div).html("<a href='" + val.url + "' title='" + val.description + "'><span class='image' style='background-image: url(" + val.url + ");'></span></a>");
+            console.log(val.description);
+            arr.push(item);
+        });
+        $(".row").append(arr);
     });
-    $(".row").append(arr);
-  });
 
-  $('.galery').magnificPopup({
-    delegate: 'a', // child items selector, by clicking on it popup will open
-    type: 'image',
-    gallery:{enabled:true}
-  });
+    $('.galery').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
 
 });
